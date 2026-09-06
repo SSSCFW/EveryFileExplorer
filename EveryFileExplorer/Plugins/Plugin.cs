d497152a0241a12f5fef0592cc0f71c5d3a4dd6e
SSSCFW <85436308+SSSCFW@users.noreply.github.com>
2026-09-06 16:03:14 +0900

    Filter malformed plugin types before Identifier lookup

--- EveryFileExplorer/Plugins/Plugin.cs
+++ EveryFileExplorer/Plugins/Plugin.cs
@@ -32,10 +32,12 @@ namespace EveryFileExplorer.Plugins
 			{
 				Version = null;
 			}
+
 			Type[] tt = Assembly.GetExportedTypes();
 			List<Type> fe = new List<Type>();
 			List<Type> ce = new List<Type>();
 			List<Type> pe = new List<Type>();
+
 			foreach (var t in tt)
 			{
 				if (!t.IsClass) continue;
@@ -44,11 +46,29 @@ namespace EveryFileExplorer.Plugins
 					Initializer = (EFEPlugin)t.InvokeMember(null, BindingFlags.CreateInstance, null, null, null);
 					continue;
 				}
-				var v = t.GetInterfaces();
-				if (v.Length != 0 && t.GetInterfaces()[0].Name == "FileFormatBase") fe.Add(t);
-				else if (v.Length != 0 && t.GetInterfaces()[0].Name == "CompressionFormatBase") ce.Add(t);
-				else if (v.Length != 0 && t.GetInterfaces()[0].Name == "ProjectBase") pe.Add(t);
+
+				var interfaces = t.GetInterfaces();
+				bool isFileFormat = interfaces.Any(i => i.Name == "FileFormatBase");
+				bool isCompression = interfaces.Any(i => i.Name == "CompressionFormatBase");
+				bool isProject = interfaces.Any(i => i.Name == "ProjectBase");
+
+				if (!isFileFormat && !isCompression && !isProject) continue;
+
+				// Form1 and FileManager expect every registered plugin type to expose
+				// a static Identifier member. Do the validation here so a malformed or
+				// dependency type can never reach a dynamic Identifier lookup and crash
+				// the application during startup.
+				object identifier;
+				if (!StaticDynamic.TryGetStaticMember(t, "Identifier", out identifier) || identifier == null)
+				{
+					continue;
+				}
+
+				if (isFileFormat) fe.Add(t);
+				else if (isCompression) ce.Add(t);
+				else if (isProject) pe.Add(t);
 			}
+
 			FileFormatTypes = fe.ToArray();
 			CompressionTypes = ce.ToArray();
 			ProjectTypes = pe.ToArray();
@@ -60,11 +80,18 @@ namespace EveryFileExplorer.Plugins
 			foreach (var t in tt)
 			{
 				if (!t.IsClass) continue;
-				var v = t.GetInterfaces();
-				if (v.Length != 0 && (t.GetInterfaces()[0].Name == "FileFormatBase" || t.GetInterfaces()[0].Name == "CompressionFormatBase" || t.GetInterfaces()[0].Name == "ProjectBase")) return true;
+
+				var interfaces = t.GetInterfaces();
+				bool isPluginType = interfaces.Any(i => i.Name == "FileFormatBase" || i.Name == "CompressionFormatBase" || i.Name == "ProjectBase");
+				if (!isPluginType) continue;
+
+				object identifier;
+				if (StaticDynamic.TryGetStaticMember(t, "Identifier", out identifier) && identifier != null)
+				{
+					return true;
+				}
 			}
 			return false;
 		}
 	}
-
 }
